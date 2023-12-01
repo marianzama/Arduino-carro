35e038a641d29241e85f3e2127008090f5c53694
marianzama <149696599+marianzama@users.noreply.github.com>
2023-12-01 00:07:33 -0600

    Update sensordelinea.ino

--- sensordelinea.ino
+++ sensordelinea.ino
@@ -1,17 +1,12 @@
-#include <Servo.h>
 #define LE 2
 #define MID 4
 #define RIG 10
-#define EC A4
-#define TR A5
 #define ML1 7
 #define ML2 8
 #define MLPWM 5
 #define MR1 11
 #define MR2 9
 #define MRPWM 6
-Servo S1;
-#define pinS 3
 int sr;
 int sm;
 int sl;
@@ -42,41 +37,6 @@ void Motorizq(float VEL){
   digitalWrite(ML1,LOW);digitalWrite(ML2,HIGH);
   digitalWrite(MR1,HIGH);digitalWrite(MR2,LOW);
 }
-void ultra(){
-  digitalWrite(TR,LOW);delay(10);
-  digitalWrite(TR,HIGH);delay(10);
-  digitalWrite(TR,LOW);
-  float di=pulseIn(EC,HIGH);
-  float d=di*0.034/2;
-  if (d>15){
-    Motoradelante(125);
-  }
-  if(d<=15){
-    S1.write(10);
-    Motorpausa();
-    if (d>7){
-      delay(1000);
-      Motorder(125);
-      delay(1000);
-      Motoradelante(125);
-      S1.write(90);
-    }if (d<=7){
-      S1.write(170);
-      if (d>7){
-        delay(1000);
-        Motorizq(125);
-        delay(1000);
-        Motoradelante(125);
-        S1.write(90);
-      }else{
-        delay(1000);
-        Motorrever(125);
-        S1.write(90);
-      }
-    }
-  }
-          
-} 
 void linea(){
   while(true){
     sr=digitalRead(RIG);sm=digitalRead(MID);sl=digitalRead(LE);
@@ -136,20 +96,15 @@ void adelante1(){
 void setup() {
   Serial.begin(9600);
   pinMode(LE,INPUT);pinMode(MID,INPUT);pinMode(RIG,INPUT);
-  pinMode(EC,INPUT);pinMode(TR,OUTPUT);
   pinMode(ML1,OUTPUT);pinMode(ML2,OUTPUT);pinMode(MLPWM,OUTPUT);
   pinMode(MR1,OUTPUT);pinMode(MR2,OUTPUT);pinMode(MRPWM,OUTPUT);
   digitalWrite(ML1,LOW);digitalWrite(ML2,LOW);
   digitalWrite(MR1,LOW);digitalWrite(MR2,LOW);
-  S1.attach(pinS);S1.write(90);
-  delay(1000);
 }
 void loop() {
   //linea();
   //lineatras();
-  ultra();
   //adelante1();
-  //blue();
   //grid1();
   //grid2();
   //grid3();
@@ -170,19 +125,4 @@ void loop() {
   // Serial.println(sr);
   // Serial.println(sm);
   // Serial.println(sl);
-//  long t; 
-//  long d; 
-//
-//  digitalWrite(TR, HIGH);
-//  delayMicroseconds(10);         
-//  digitalWrite(TR, LOW);
-//  
-//  t = pulseIn(EC, HIGH); 
-//  d = t/59;             
-//  
-//  Serial.print("Distancia: ");
-//  Serial.print(d);
-//  Serial.print("cm");
-//  Serial.println();
-//  delay(100); 
 }
